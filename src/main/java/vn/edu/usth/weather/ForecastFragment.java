84b06324646efdee4925bcf25acdfeeea0b7cc42
Tieuphi99 <tieuphi99@gmail.com>
2019-11-11 14:04:14 +0700

    Practical work 4

---
+++ src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -0,0 +1,35 @@
+package vn.edu.usth.weather;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class ForecastFragment extends Fragment {
+    public ForecastFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = new View(getContext());
+        v.setBackgroundColor(0xFFF69696);
+        LinearLayout ll = new LinearLayout(getContext());
+        ll.setOrientation(LinearLayout.VERTICAL);
+        return v;
+    }
+}
---
+++ src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,56 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class WeatherActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private TextView mytextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("create", "Noice");
+        setContentView(R.layout.activity_weather);
+        // Create a new Fragment to be placed in the activity layout
+        ForecastFragment firstFragment = new ForecastFragment();
+        // Add the fragment to the 'container' FrameLayout
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.container, firstFragment).commit();
+        this.mytextView = (TextView)this.findViewById(R.id.mytextView);
+        this.imageView = (ImageView)this.findViewById(R.id.imageView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("start", "Cool");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("resume", "Very cool");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("pause", "Not very cool");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("stop", "Not cool");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("destroy", "Ur ded");
+    }
+}
