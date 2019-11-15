e067baaa99f90735a084a0aa0c80c74c9c7aeba1
Tieuphi99 <tieuphi99@gmail.com>
2019-11-15 13:29:14 +0700

    Update practical work 4

--- src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -21,4 +21,5 @@ public class ForecastFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_forecast, container, false);
     }
+
 }
