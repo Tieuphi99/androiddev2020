988159ed8414076acf59cee25b70bf2c85d2e5b7
Tieuphi99 <tieuphi99@gmail.com>
2019-11-17 15:17:57 +0700

    Practical work 7

---
+++ src/main/java/vn/edu/usth/weather/HomeFragmentPagerAdapter.java
@@ -0,0 +1,43 @@
+package vn.edu.usth.weather;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+    private final int PAGE_COUNT = 3;
+    private String titles[] = new String[]{"Hanoi", "Paris", "Toulouse"};
+
+    public HomeFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT; // number of pages for a ViewPager
+    }
+
+    @Override
+    public Fragment getItem(int page) {
+// returns an instance of Fragment corresponding to the specified page
+        switch (page) {
+            case 0:
+                WeatherAndForecastFragment Fragment1;
+                return Fragment1 = new WeatherAndForecastFragment();
+            case 1:
+                WeatherAndForecastFragment Fragment2;
+                return Fragment2 = new WeatherAndForecastFragment();
+            case 2:
+                WeatherAndForecastFragment Fragment3;
+                return Fragment3 = new WeatherAndForecastFragment();
+        }
+        WeatherAndForecastFragment EmptyFragment;
+        return EmptyFragment = new WeatherAndForecastFragment();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int page) {
+// returns a tab title corresponding to the specified page
+        return titles[page];
+    }
+}
--- src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,6 +1,12 @@
 package vn.edu.usth.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.content.Context;
 import android.os.Bundle;
 import android.util.AttributeSet;
@@ -9,6 +15,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class WeatherActivity extends AppCompatActivity {
 
     @Override
@@ -16,14 +24,30 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.i("create", "Noice");
         setContentView(R.layout.activity_weather);
-        // Create a new Fragment to be placed in the activity layout
+//         Create a new Fragment to be placed in the activity layout
         WeatherFragment secondFragment = new WeatherFragment();
         ForecastFragment firstFragment = new ForecastFragment();
-        // Add the fragment to the 'container' FrameLayout
+        WeatherAndForecastFragment Fragment1 = new WeatherAndForecastFragment();
+        WeatherAndForecastFragment Fragment2 = new WeatherAndForecastFragment();
+        WeatherAndForecastFragment Fragment3 = new WeatherAndForecastFragment();
+//         Add the fragment to the 'container' FrameLayout
         getSupportFragmentManager().beginTransaction().add(
                 R.id.container, secondFragment).commit();
         getSupportFragmentManager().beginTransaction().add(
                 R.id.container, firstFragment).commit();
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.container, Fragment1).commit();
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.container, Fragment2).commit();
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.container, Fragment3).commit();
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(
+                getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
+        tabLayout.setupWithViewPager(pager);
     }
 
     @Override
